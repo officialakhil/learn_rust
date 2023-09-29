40c732ff5f7272c32ae5506992ee1f03b211730a
Akhil <akhilesh.tulluri@gmail.com>
2023-09-29 16:40:52 +0530

    chore(errors): don't panic when empty arrays

--- median/src/main.rs
+++ median/src/main.rs
@@ -1,6 +1,8 @@
-fn merge_and_find_median(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+use std::io::ErrorKind;
+
+fn merge_and_find_median(nums1: Vec<i32>, nums2: Vec<i32>) -> Result<f64, ErrorKind> {
     if nums1.is_empty() && nums2.is_empty() {
-        return 0.0;
+        return Err(ErrorKind::InvalidInput);
     }
 
     let mut nums: Vec<i32> = nums1.into_iter().chain(nums2.into_iter()).collect();
@@ -11,40 +13,50 @@ fn merge_and_find_median(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
 
     if len % 2 == 0 {
         let mid = len / 2;
-        (nums[mid] + nums[mid - 1]) as f64 / 2.0
+        Ok((nums[mid] + nums[mid - 1]) as f64 / 2.0)
     } else {
-        nums[len / 2] as f64
+        Ok(nums[len / 2] as f64)
     }
 }
 
 fn main() {
     let nums1 = vec![1, 3, 2, 6, -4];
     let nums2 = vec![2, 9, 1, 4, 5];
-    println!("{}", merge_and_find_median(nums1, nums2));
+    println!(
+        "Median of two arrays is {}",
+        merge_and_find_median(nums1, nums2).unwrap()
+    );
 }
 
 #[cfg(test)]
 mod tests {
-    use super::merge_and_find_median;
+    use super::*;
 
     #[test]
-    fn test_odd() {
+    fn test_odd() -> Result<(), ErrorKind> {
         let nums1 = vec![1, 3, 2, 6, -4];
         let nums2 = vec![2, 9, 1, 4];
-        assert_eq!(merge_and_find_median(nums1, nums2), 2.0);
+        assert_eq!(merge_and_find_median(nums1, nums2)?, 2.0);
+
+        Ok(())
     }
 
     #[test]
-    fn test_even() {
+    fn test_even() -> Result<(), ErrorKind> {
         let nums1 = vec![1, 3, 2, 6, -4];
         let nums2 = vec![2, 9, 1, 4, 5];
-        assert_eq!(merge_and_find_median(nums1, nums2), 2.5);
+        assert_eq!(merge_and_find_median(nums1, nums2)?, 2.5);
+
+        Ok(())
     }
 
     #[test]
-    fn test_empty() {
-        let nums1 = vec![];
-        let nums2 = vec![];
-        assert_eq!(merge_and_find_median(nums1, nums2), 0.0);
+    fn test_empty() -> Result<(), ErrorKind> {
+        assert_eq!(
+            merge_and_find_median(vec![], vec![]),
+            Err(ErrorKind::InvalidInput)
+        );
+
+        Ok(())
     }
 }
